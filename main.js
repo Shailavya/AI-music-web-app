23251ffed2220eae405e25fd009c60cc52f6ce8f
Shailavya <76102224+Shailavya@users.noreply.github.com>
2021-09-23 15:02:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function preload() {
     song = loadSound("music.mp3");
+
 }
 
 function setup() {
@@ -8,15 +9,74 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotPoses);
 }
 var song = "";
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
+var inNumberleftWristY = 0;
+var leftWristY_divide_1000 = 0;
+var volume = 0;
+var scoreLeftWrist = 0;
+var remove_decimals = 0;
+ 
 
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+    inNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(inNumberleftWristY);
+    leftWristY_divide_1000 = remove_decimals/1000;
+    volume = leftWristY_divide_1000 *2;
+    document.getElementById("volume").innerHTML = 'Volume = ' + volume;
+    song.setVolume(volume);
+    }
+    
+    
+
 }
 
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist X = " + leftWristX + "Left wrist Y = " + leftWristY);
+
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist X = " + rightWristX + "Right wrist Y = " + rightWristY);
+    }
+}
+
+function stop() {
+    song.pause();
 }
